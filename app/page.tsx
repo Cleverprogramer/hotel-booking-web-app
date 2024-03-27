783964ae7761dfb9f5a9e8145d77076f759ccde3
Cleverprogramer <cleverprogrammer.com@gmail.com>
2024-03-27 19:14:14 +0300

    removing unwanted files.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h2>Welcome Hotel booking web app</h2>
+      <h2 className="text-xl text-center">Welcome Hotel booking web app</h2>
     </>
   );
 }
