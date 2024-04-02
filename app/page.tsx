da0c2928925a7cef8832d7bdd05baf842af9b9f7
Cleverprogramer <cleverprogrammer.com@gmail.com>
2024-04-02 21:28:58 +0300

    Adding Section Client saying

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import HomePageHeroSection from "@/components/Pages/Home/SectionHero";
+import SectionHowItWork from "@/components/HowItWorks";
+import SectionClientSay from "@/components/Pages/Home/ClientSay";
+import BackgroundSection from "@/components/shared/BackgroundSection";
 
 const HomePage = () => {
   return (
     <main className="nc-PageHome relative overflow-hidden">
       <div className="container relative space-y-24 mb-24 lg:space-y-28 lg:mb-28">
         <HomePageHeroSection className="" />
+        <SectionHowItWork />
+        <div className="relative py-16">
+          <BackgroundSection />
+          <SectionClientSay />
+        </div>
       </div>
     </main>
   );
