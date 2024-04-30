5643fa012b90e64403e6a5a7d9d8b5560c779e87
cleverprogrammer <cleverprogrammer.com@gmail.com>
2024-04-30 17:28:33 +0300

    Adding calender switching

---
+++ utils/DateFormating.ts
@@ -0,0 +1,25 @@
+interface IDateFormating {
+  CheckIn: string;
+  CheckOut: string;
+}
+
+export const DateFormating = ({ CheckIn, CheckOut }: IDateFormating) => {
+  const FormatingCheckingDate = CheckIn.split("/");
+  const FormatingCheckOutDate = CheckOut.split("/");
+
+  const CheckingMonth = FormatingCheckingDate[1];
+  const CheckingDay = FormatingCheckingDate[0];
+  const CheckingYear = FormatingCheckingDate[2];
+
+  const CheckOutMonth = FormatingCheckOutDate[1];
+  const CheckOutDay = FormatingCheckOutDate[0];
+  const CheckOutYear = FormatingCheckOutDate[2];
+
+  const CheckInDate: any = `${CheckingMonth}/${CheckingDay}/${CheckingYear}`;
+  const CheckOutDate: any = `${CheckOutMonth}/${CheckOutDay}/${CheckOutYear}`;
+
+  const diff: any = Math.abs(new Date(CheckOutDate) - new Date(CheckInDate));
+  const dayscount = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+  return dayscount;
+};
