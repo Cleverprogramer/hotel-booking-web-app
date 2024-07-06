51a864b2a17dc398dbaccfd2bb0d4d8fd41bd692
cleverprogrammer <cleverprogrammer.com@gmail.com>
2024-07-05 19:40:05 -0700

    adding react hook form and setting up the inputs and also getting the values by using zod and react hook form.

--- components/Pages/auth/LoginForm.tsx
+++ components/Pages/auth/LoginForm.tsx
@@ -6,16 +6,32 @@ import Link from "next/link";
 import Input from "@/components/shared/Input";
 import ButtonPrimary from "@/components/shared/Button/ButtonPrimary";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { LoginSchema } from "@/schema/Login";
+
 const LoginForm = () => {
+  const { register, handleSubmit } = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof LoginSchema>) {
+    console.log(values);
+  }
+
   return (
     <div>
-      <form className="grid grid-cols-1 gap-6">
+      <form
+        className="grid grid-cols-1 gap-6"
+        onSubmit={handleSubmit((data) => onSubmit(data))}>
         <label className="block">
           <span className="text-neutral-800 dark:text-neutral-200">
             Email address
           </span>
           <Input
             type="email"
+            {...register("email")}
             placeholder="example@example.com"
             className="mt-1"
           />
@@ -27,7 +43,7 @@ const LoginForm = () => {
               Forgot password?
             </Link>
           </span>
-          <Input type="password" className="mt-1" />
+          <Input type="password" className="mt-1" {...register("password")} />
         </label>
         <ButtonPrimary type="submit">Continue</ButtonPrimary>
       </form>
--- components/Pages/auth/SingupForm.tsx
+++ components/Pages/auth/SingupForm.tsx
@@ -5,16 +5,31 @@ import React from "react";
 import Input from "@/components/shared/Input";
 import ButtonPrimary from "@/components/shared/Button/ButtonPrimary";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { SingupSchema } from "@/schema/signup";
+
 const SingupForm = () => {
+  const { register, handleSubmit } = useForm<z.infer<typeof SingupSchema>>({
+    resolver: zodResolver(SingupSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof SingupSchema>) {
+    console.log(values);
+  }
   return (
     <div>
-      <form className="grid grid-cols-1 gap-6" action="#" method="post">
+      <form
+        className="grid grid-cols-1 gap-6"
+        onSubmit={handleSubmit((data) => onSubmit(data))}>
         <label className="block">
           <span className="text-neutral-800 dark:text-neutral-200">
             Email address
           </span>
           <Input
             type="email"
+            {...register("email")}
             placeholder="example@example.com"
             className="mt-1"
           />
@@ -23,7 +38,7 @@ const SingupForm = () => {
           <span className="flex justify-between items-center text-neutral-800 dark:text-neutral-200">
             Password
           </span>
-          <Input type="password" className="mt-1" />
+          <Input type="password" {...register("password")} className="mt-1" />
         </label>
         <ButtonPrimary type="submit">Continue</ButtonPrimary>
       </form>
