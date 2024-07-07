f5d2de1a4ad7decec758225325af032c3b65bdc1
cleverprogrammer <cleverprogrammer.com@gmail.com>
2024-07-06 20:26:26 -0700

    working and making function in oauth providers like github and google providers.

--- components/Pages/auth/authProvider.tsx
+++ components/Pages/auth/authProvider.tsx
@@ -1,18 +1,28 @@
+"use client";
 import React from "react";
 
 import { FcGoogle } from "react-icons/fc";
 import { LuGithub } from "react-icons/lu";
 
+import { signIn } from "next-auth/react";
+
 const AuthProvider = () => {
+  const HandleOauth = (provider: "google" | "github") => {
+    signIn(provider);
+  };
   return (
     <div className="grid gap-3">
-      <button className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]">
+      <button
+        className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]"
+        onClick={() => HandleOauth("google")}>
         <FcGoogle className="text-xl" />
         <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
           Continue with Google
         </h3>
       </button>
-      <button className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]">
+      <button
+        className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]"
+        onClick={() => HandleOauth("github")}>
         <LuGithub className="text-xl" />
         <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
           Continue with Github
