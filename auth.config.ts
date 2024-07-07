aac5b0a1da5e0fdb83c6329b38458aa40b2a269d
cleverprogrammer <cleverprogrammer.com@gmail.com>
2024-07-07 11:33:42 -0700

    Making ready next auth for login email and password or credentials provider.

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,12 @@
+import type { NextAuthConfig } from "next-auth";
+
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+import { LoginSchema } from "./schema/Login";
+import { getUserByEmail } from "./constants/getUserByEmail";
+import bcrypt from "bcryptjs";
 
 export default {
   providers: [
@@ -11,6 +17,26 @@ export default {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validating = LoginSchema.safeParse(credentials);
+
+        if (validating.success) {
+          const { email, password } = credentials;
+          const existingUser = await getUserByEmail(email as string);
+
+          if (!existingUser || !existingUser.password) return null;
+
+          const passwordMatch = await bcrypt.compare(
+            password as string,
+            existingUser.password
+          );
+
+          if (passwordMatch) return existingUser;
+        }
+        return null;
+      },
     }),
   ],
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 import prisma from "./utils/db";
