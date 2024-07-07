6c19205c40254a216fb522de28b7582495eb674a
cleverprogrammer <cleverprogrammer.com@gmail.com>
2024-07-07 11:35:17 -0700

    working and making function in server action like singup the user and login the user with emial and password with next auth.

--- server/auth/Singup.ts
+++ server/auth/Singup.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { getUserByEmail } from "@/constants/getUserByEmail";
 import { SingupSchema } from "@/schema/signup";
+import prisma from "@/utils/db";
 import { wait } from "@/utils/wait";
 import { z } from "zod";
+import bcrypt from "bcryptjs";
 
 export const SingupUser = async (data: z.infer<typeof SingupSchema>) => {
   const checking = SingupSchema.safeParse(data);
@@ -11,7 +14,24 @@ export const SingupUser = async (data: z.infer<typeof SingupSchema>) => {
     return { error: "Please fill the form" };
   }
 
+  const { email, password } = checking.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "User already exists" };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   wait(1000);
 
-  return { success: "Welcome to your account" };
+  try {
+    const createUser = await prisma.user.create({
+      data: { email, password: hashedPassword },
+    });
+    return { success: "Your account created successfully!" };
+  } catch (error: any) {
+    return { error: error?.message };
+  }
 };
--- server/auth/login.ts
+++ server/auth/login.ts
@@ -1,7 +1,11 @@
 "use server";
 
+import { signIn } from "@/auth";
+import { getUserByEmail } from "@/constants/getUserByEmail";
+import { DefaultRedirectUrl } from "@/routes";
 import { LoginSchema } from "@/schema/Login";
 import { wait } from "@/utils/wait";
+import { AuthError } from "next-auth";
 import { z } from "zod";
 
 export const LoginUser = async (data: z.infer<typeof LoginSchema>) => {
@@ -11,7 +15,31 @@ export const LoginUser = async (data: z.infer<typeof LoginSchema>) => {
     return { error: "Please fill the form" };
   }
 
+  const { email, password } = checking.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser) {
+    return { error: "user not found" };
+  }
+
   wait(1000);
 
-  return { success: "Welcome to your account" };
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DefaultRedirectUrl,
+    });
+  } catch (error: any) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CallbackRouteError":
+          return { error: "Invalid Credentials" };
+        default:
+          return { error: "Something went wrong" };
+      }
+    }
+    throw error;
+  }
 };
