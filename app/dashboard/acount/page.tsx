ad900d68ee0ef907c74159feaca00ca4fd67306c
cleverprogrammer <cleverprogrammer.com@gmail.com>
2024-07-09 19:27:18 -0700

    adding dashboard acount page

---
+++ app/dashboard/acount/page.tsx
@@ -0,0 +1,8 @@
+import AcountPageComponent from "@/components/Pages/Acount/AcountPage";
+import React from "react";
+
+const AcountPage = () => {
+  return <AcountPageComponent />;
+};
+
+export default AcountPage;
