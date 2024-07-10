502492de30a4b2e202fad8ae5475ba10ef217a5d
cleverprogrammer <cleverprogrammer.com@gmail.com>
2024-07-09 19:39:24 -0700

    Adding users typescript

---
+++ types/User.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+  id: string;
+  email: string | null;
+  name: string;
+  gender: string | null;
+  image: string | null;
+  address: string | null;
+  aboutYou: string | null;
+  dateOfBrith: string | null;
+  phoneNumber: string | null;
+  username: string | null;
+}
