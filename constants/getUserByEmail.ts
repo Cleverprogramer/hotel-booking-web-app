a102e8600e49c331f95ba79639491cdc97443ca2
cleverprogrammer <cleverprogrammer.com@gmail.com>
2024-07-09 19:38:52 -0700

    adding getUserById helper for users

--- constants/getUserByEmail.ts
+++ constants/getUserByEmail.ts
@@ -4,3 +4,8 @@ export const getUserByEmail = async (email: string) => {
   const ExistingUser = await prisma.user.findUnique({ where: { email } });
   return ExistingUser;
 };
+
+export const getUserById = async (id: string) => {
+  const ExistingUser = await prisma.user.findUnique({ where: { id } });
+  return ExistingUser;
+};
