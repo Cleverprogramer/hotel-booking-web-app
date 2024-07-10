2ad3e84bea83c87fce5c4331ae256d1505dcfa8a
cleverprogrammer <cleverprogrammer.com@gmail.com>
2024-07-09 19:24:18 -0700

    adding next auth session callbacks with typescript

---
+++ next-auth.d.ts
@@ -0,0 +1,14 @@
+import NextAuth, { type DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      username: string;
+      role: "admin" | "user";
+      address: string;
+    } & DefaultSession["user"];
+  }
+}
